213df3d7fe4d3dd738c2c1bb3a995edfc8f1fe79
ConvitePremium <essenciamusicalofc@gmail.com>
2026-09-18 16:18:43 -0300

    Update config.js

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 window.EVENTO_CONFIG = {
   nome: "Thaís",
   idade: "15 anos",
-  botaoPrincipal: { x: 20, y: 65, largura: 60, altura: 12 },
+  botaoPrincipal: { x: 22.4, y: 50.8, largura: 55.1, altura: 12 },
   moldura: {
     arquivo: "assets/moldura.png",
     nomeArquivo: "foto-thais.png"
